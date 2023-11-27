2d2952a1049dcc4ff6aa6a78688f72d38b3e2031
Ramachandra-2096 <ramachandraudupa2004@gmail.com>
2023-11-27 22:42:02 +0530

    changed

--- 8th.cpp
+++ 8th.cpp
@@ -1,16 +1,13 @@
 #include <iostream>
 using namespace std;
 int main() {
-    int baseAddress = 100;
+    int baseAdd = 100;
     int rowSize = 10;
     int columnSize = 15;
     int elementSize = 1; 
     int rowIndex = 8;
     int columnIndex = 6;
-
-    int address = baseAddress + (columnSize * (rowIndex + (columnIndex * rowSize))) * elementSize;
-
+    int address = baseAdd + (columnSize * (rowIndex + (columnIndex * rowSize))) * elementSize;
     cout << "Address of arr[8][6] in column-major order: " << address;
-
     return 0;
 }
